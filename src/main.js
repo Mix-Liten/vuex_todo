096430210123e77f3f615c39cd6b8b19513d2dbf
ianchang <ian.chang@dev.cctech-support.com>
2019-07-17 16:56:11 +0800

    add vueRouter

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from './_axios'
 import store from './store'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  axios,
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
